bf7d7b45d74f8f6d9235a50e25cbff4ca891dcc5
Marisa Richmond <marisa.richmond94@gmail.com>
2021-11-06 12:25:49 -0500

    More Realistic Mock Data
    For now, I've cut down the locations to just California and the jobs to just those related to software engineering (to make the data set size more realistic)

--- src/routes/components/search/input/dropdown/index.tsx
+++ src/routes/components/search/input/dropdown/index.tsx
@@ -1,12 +1,25 @@
 import { ReactElement } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import GigDropdown from 'components/gig_input/dropdown';
+import settings from 'settings';
+
 import { Option, SearchDropdownInputProps } from './types';
 
 const SearchDropdownInput = (props: SearchDropdownInputProps): ReactElement => {
+  const { pathname } = useLocation();
+
+  const getClassNames = (): string => {
+    let classNames = props.value?.displayName ? '' : 'placeholder-text';
+    classNames += pathname === settings.SEARCH_ROUTE
+      ? 'off-white-gig-dropdown'
+      : 'white-gig-dropdown';
+    return classNames;
+  };
+
   return (
     <GigDropdown
-      classNames={`off-white-gig-dropdown${props.value?.displayName ? '' : ' placeholder-text'}`}
+      classNames={getClassNames()}
       id='search-form-type-input'
       onOptionSelect={(option: Option): void => props.updateInput(props.fieldName, option)}
       options={props.options}
