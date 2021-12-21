571d1d8927072c2c0c58720b2916dcb8d02b129c
Marisa Richmond <marisa.richmond94@gmail.com>
2021-12-21 10:47:14 -0600

    xQ4lMWWk: Delete Search Inputs

--- src/routes/components/search/panel/index.tsx
+++ src/routes/components/search/panel/index.tsx
@@ -3,11 +3,11 @@ import './index.scss';
 import { ReactElement, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import GigDropdown from 'components/gig_input/dropdown';
+import ControlledSearchableGigInput from 'components/gig_input/searchable/controlled';
 import { useViewport } from 'hooks/useViewport';
 import useQuery from 'hooks/useQuery';
 import { useSearch } from 'providers/search';
-import SearchTextInput from 'routes/components/search/input/text';
-import SearchDropdownInput from 'routes/components/search/input/dropdown';
 import settings from 'settings';
 import { DropdownOption } from 'types';
 
@@ -59,35 +59,46 @@ const SearchPanel = (): ReactElement => {
     return `${pageType} search-panel-item search-panel-input ${route}`;
   }
 
+  const getDropdownClassNames = (selectedOption: DropdownOption): string => {
+    let classNames = selectedOption?.displayName ? '' : 'placeholder-text';
+    classNames += pathname === settings.FIND_ROUTE
+      ? ' off-white-gig-dropdown'
+      : ' white-gig-dropdown';
+    return classNames;
+  };
+
   return (
     <div id={`${pathname === settings.FIND_ROUTE ? 'find-' : 'home-'}search-panel`}>
       <div className={getClassNames()}>
-        <SearchTextInput
-          fieldName='title'
+        <ControlledSearchableGigInput
+          classNames='search-form-input'
+          formValue={title}
           id='search-text-title-input'
           options={titleOptions}
           placeholder='tech job title (e.g. "Software Engineer")'
-          updateSearchText={updateSearchText}
-          value={title}
+          onChange={(updatedTitle: string) => updateSearchText('title', updatedTitle)}
+          onOptionSelect={(updatedTitle: string) => updateSearchText('title', updatedTitle)}
         />
       </div>
       <div className={getClassNames()}>
-        <SearchTextInput
-          fieldName='location'
+        <ControlledSearchableGigInput
+          classNames='search-form-input'
+          formValue={location}
           id='search-text-location-input'
           options={locationOptions}
           placeholder='city in California (e.g. "Los Angeles")'
-          updateSearchText={updateSearchText}
-          value={location}
+          onChange={(updatedCity: string) => updateSearchText('location', updatedCity)}
+          onOptionSelect={(updatedCity: string) => updateSearchText('location', updatedCity)}
         />
       </div>
       <div className={getClassNames()}>
-        <SearchDropdownInput
-          fieldName='type'
+        <GigDropdown
+          classNames={getDropdownClassNames(type ? { displayName: type } : undefined)}
+          id='search-form-type-input'
+          onOptionSelect={(selectedType: DropdownOption) => updateSelectedOption(selectedType)}
           options={typeOptions?.map(typeOption => { return { displayName: typeOption }; }) || []}
           placeholder='full-time, part-time, etc.'
           selectedOption={type ? { displayName: type } : undefined}
-          updateInput={updateSelectedOption}
         />
       </div>
     </div>
